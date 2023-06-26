a2e9c6f8aea8f6acfe44d6ba99f159ac4572cc57
shmjade <shm.jade@gmail.com>
2023-06-26 17:32:28 -0300

    make cpf optional

--- src/bank_api/logic.clj
+++ src/bank_api/logic.clj
@@ -4,13 +4,13 @@
   (re-matches #"\d{3}\.\d{3}\.\d{3}\-\d{2}" cpf))
 
 (defn new-account
-  [{:keys [name cpf]}]
-  (if (valid-cpf? cpf)
-    {:id (random-uuid)
-     :name name
-     :cpf cpf
-     :balance 0M}
-    (throw (Exception. "Invalid cpf"))))
+  [{:keys [name cpf]}] 
+  (let [account     {:id (random-uuid)
+                     :name name
+                     :balance 0M}]
+    (if (nil? cpf) account
+        (if (valid-cpf? cpf) (assoc account :cpf cpf)
+            (throw (Exception. "Invalid cpf"))))))
 
 (defn deposit
   [account
